9234ee71ac2445c862c18327214f221c9b624002
jackknife <jackknife@jackknife-Lenovo-S10-3c.(none)>
2013-04-16 22:57:54 +0530

    curtainjs first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Curtainjs::Application.config.secret_token = 'ab22bd0c909b514f0194551592360dd19eb25c9d7e218ff45806bde3afade49c29d85e705f4fb0ed03f2d2f86fb743e0c94ae35e9d2a1404deeea3a76666c5b2'
